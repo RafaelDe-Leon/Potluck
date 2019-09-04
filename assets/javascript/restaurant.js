dd7f94a89c41891e8dfac12de9b8eaed21721e25
Kayode Oseni <k.oseni.jr@gmail.com>
2019-09-04 12:59:40 -0400

    added html skeleton, enter key functionality, and google map. Intend on working on zomato api now.

---
+++ assets/javascript/restaurant.js
@@ -0,0 +1,21 @@
+$(function(){
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+
+    $(window).resize(function(e) {
+      if($(window).width()<=768){
+        $("#wrapper").removeClass("toggled");
+      }else{
+        $("#wrapper").addClass("toggled");
+      }
+    });
+  });
+  //  create a keypress method for the enter key
+  $("#inputZip.form-control").keypress(function(event) {
+    let key = event.keyCode;
+      if(key === 13) {
+        console.log("ENTER");
+      }
+    })
