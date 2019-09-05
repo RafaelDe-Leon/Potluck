433f090e4be10989c1ccb93f6f0d1b0a61106864
John Harper <jharper1@tulane.edu>
2019-09-05 17:10:21 -0400

    News API test page

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 
+$("#submit-name").on("click", function(event) {
+    event.preventDefault();
+    // Get the input values
+    var restaurantName = $("#restaurantName").val().trim();
+
 
 var url = 'https://newsapi.org/v2/everything?' +
 'q=' + restaurantName +
-'from=2019-09-05&' +
+'&from=2019-09-05&' +
 'sortBy=popularity&' +
 'apiKey=bfc8e374d6df45af85688db28a5bf373';
 
@@ -12,4 +17,4 @@ fetch(req)
 .then(function(response) {
 console.log(response.json());
 })
-
+})
