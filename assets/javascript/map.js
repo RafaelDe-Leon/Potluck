88e18a65376254231a5c92b3759b30fc7a152226
Rafael De Leon <rafael_de_leon88@hotmail.com>
2019-09-09 17:25:59 -0400

    fixed a couple things

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -1,39 +1,45 @@
-// Firebase for Apps
-// Your web app's Firebase configuration
-// var firebaseConfig = {
-//   apiKey: "AIzaSyDZ14hkAIfRW7aJMS61i1gUBBsausAImfw",
-//   authDomain: "restaurant-hype.firebaseapp.com",
-//   databaseURL: "https://restaurant-hype.firebaseio.com",
-//   projectId: "restaurant-hype",
-//   storageBucket: "",
-//   messagingSenderId: "328234147973",
-//   appId: "1:328234147973:web:8006c605ff9b7eadb409ec"
-// };
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
-
 //global variable to hold map
-let map;
-let mapApiKey = AIzaSyAM69XHYamrYzng19RSBBgUFIzDWmZ_6Y0;
-let mapUrl =
-  "https://maps.googleapis.com/maps/api/js?key=" +
-  mapApiKey +
-  "&callback=initMap";
+// let map;
+// let mapApiKey = "AIzaSyAM69XHYamrYzng19RSBBgUFIzDWmZ_6Y0";
+// let mapUrl =
+//   "https://maps.googleapis.com/maps/api/js?key=" +
+//   mapApiKey +
+//   "&callback=initMap";
 
-console.log(mapUrl);
+// console.log(mapUrl);
 
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
+// $.ajax({
+//   url: mapUrl,
+//   method: "GET"
+// }).then(function(response) {
 
-  
+  // function that ingnites the map
+  // function initMap() {
+  //   map = new google.maps.Map(document.getElementById("map"), {
+  //     center: { lat: -34.397, lng: 150.644 },
+  //     zoom: 8
+  //   });
+  // }
+// });
+
+
+// Get the input field
+let input = document.getElementById("inputZip");
+let idPressed = $("#show-example");
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keydown", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("inputZip").click();
+    console.log("i have been pressed");
+idPressed.append("<div class='row'>");
+idPressed.append("<div class='col-md-3'>");
+idPressed.append("<h1 class='main-color-yellow'> Hello </h1>");
+
+
+  }
 });
-
-// function that ingnites the map
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8
-  });
-}
