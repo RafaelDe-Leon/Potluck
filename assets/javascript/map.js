c46830fff73d1fd81f70364ccb6a006c1dda9555
Rafael De Leon <rafael_de_leon88@hotmail.com>
2019-09-09 18:30:59 -0400

    added column when entered is pressed on the zipcode button

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -24,22 +24,3 @@
 
 
 // Get the input field
-let input = document.getElementById("inputZip");
-let idPressed = $("#show-example");
-
-// Execute a function when the user releases a key on the keyboard
-input.addEventListener("keydown", function(event) {
-  // Number 13 is the "Enter" key on the keyboard
-  if (event.keyCode === 13) {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Trigger the button element with a click
-    document.getElementById("inputZip").click();
-    console.log("i have been pressed");
-idPressed.append("<div class='row'>");
-idPressed.append("<div class='col-md-3'>");
-idPressed.append("<h1 class='main-color-yellow'> Hello </h1>");
-
-
-  }
-});
