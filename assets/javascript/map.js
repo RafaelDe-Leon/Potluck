076022b17894b7a2adadf08d9076718db15a6d2c
Rafael De Leon <rafael_de_leon88@hotmail.com>
2019-09-09 23:48:04 -0400

    added map and a zipcode converter to langitute to the map.js

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -1,27 +1,37 @@
 //global variable to hold map
-// let map;
-// let mapApiKey = "AIzaSyAM69XHYamrYzng19RSBBgUFIzDWmZ_6Y0";
-// let mapUrl =
-//   "https://maps.googleapis.com/maps/api/js?key=" +
-//   mapApiKey +
-//   "&callback=initMap";
+let map;
+let mapApiKey = AIzaSyAM69XHYamrYzng19RSBBgUFIzDWmZ_6Y0;
+let mapUrl =
+  "https://maps.googleapis.com/maps/api/js?key=" +
+  mapApiKey +
+  "&callback=initMap";
+// grab the zipcode
+let zipcode;
+// zipcode key from converter
+let zipcodeKey =
+  "8FiYHj3y9Don2RS4h6Q3q72bywLcJFAOWdzrASt2Dch9LA0gsRE3V9OGwTuNTFoC";
 
-// console.log(mapUrl);
+// url from zipcode converter
+let zipCodeConvert =
+  "https://www.zipcodeapi.com/rest/" +
+  zipcodeKey +
+  "/info.json/" +
+  zipcode +
+  "/degrees";
 
-// $.ajax({
-//   url: mapUrl,
-//   method: "GET"
-// }).then(function(response) {
+console.log(mapUrl);
 
-  // function that ingnites the map
-  // function initMap() {
-  //   map = new google.maps.Map(document.getElementById("map"), {
-  //     center: { lat: -34.397, lng: 150.644 },
-  //     zoom: 8
-  //   });
-  // }
-// });
+// function that ingnites the map
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8
+  });
+}
 
+// zipcode to lang convert API
 
-// Get the input field
-  
+// https://www.zipcodeapi.com/rest/<api_key>/info.<format>/<zip_code>/<units>
+
+// api key
+// 8FiYHj3y9Don2RS4h6Q3q72bywLcJFAOWdzrASt2Dch9LA0gsRE3V9OGwTuNTFoC
