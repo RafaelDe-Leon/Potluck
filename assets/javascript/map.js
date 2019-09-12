ec3e0ef05e6b15690272e458c7687c324dcc873e
Rafael De Leon <rafael_de_leon88@hotmail.com>
2019-09-11 23:14:08 -0400

    getting there with the map

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -1,10 +1,11 @@
 $.ajaxPrefilter(function(options) {
   if (options.crossDomain && $.support.cors) {
-      options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+    options.url = "https://cors-anywhere.herokuapp.com/" + options.url;
   }
 });
 
 //global variable to hold map
+let idPressed = $("#column-one");
 var currentZipCode = "";
 let map;
 let mapApiKey = "AIzaSyAM69XHYamrYzng19RSBBgUFIzDWmZ_6Y0";
@@ -47,7 +48,15 @@ $("#zipForm").submit(function(event) {
     return;
   }
 
- var zipCodeUrl = buildZipCodeRequestURI(currentZipCode);
+  console.log("Zipcode Input was pressed");
+  $("#imageAreaTwo").addClass("area-two");
+  $("#column-one").addClass("column-one");
+
+  // may need later for future code
+  // idPressed.append("<div class='row'>");
+  // idPressed.append("<div class='col-md-4'");
+
+  var zipCodeUrl = buildZipCodeRequestURI(currentZipCode);
 
   // ajax call
   $.ajax({
@@ -57,7 +66,8 @@ $("#zipForm").submit(function(event) {
     console.log(response.lat);
     console.log(response.lng);
 
-    
+    let lat = response.lat;
+    let lng = response.lng;
   });
 });
 // document.getElementById("zipForm");
@@ -69,15 +79,26 @@ $("#zipForm").submit(function(event) {
 //   console.log(event.target.elements[0].value, "value");
 // };
 
-// function that ingnites the map
+$(document).on("click", ".news-button", function() {
+
+
+  $("#map-container").html("<div id='map' class=`col-md-6`></div>");
+
+});
+
+// function that ignites the map
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8
+  // ajax call
+  $.ajax({
+    url: mapUrl,
+    method: "GET"
+  }).then(function(response) {
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: 40.758614, lng: -73.967704 },
+      zoom: 8
+    });
   });
 }
-
-initMap();
 
 // zipcode to lang convert API
 
