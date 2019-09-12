e2188dd58810e436ca6030a63230b5bb17217ee1
Rafael De Leon <rafael_de_leon88@hotmail.com>
2019-09-11 21:09:53 -0400

    added map js

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -1,53 +1,85 @@
+$.ajaxPrefilter(function(options) {
+  if (options.crossDomain && $.support.cors) {
+      options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+  }
+});
+
 //global variable to hold map
+var currentZipCode = "";
 let map;
 let mapApiKey = "AIzaSyAM69XHYamrYzng19RSBBgUFIzDWmZ_6Y0";
 let mapUrl =
   "https://maps.googleapis.com/maps/api/js?key=" +
   mapApiKey +
   "&callback=initMap";
-// grab the zipcode
-let zipcode;
-// zipcode key from converter
-let zipcodeKey =
-  "8FiYHj3y9Don2RS4h6Q3q72bywLcJFAOWdzrASt2Dch9LA0gsRE3V9OGwTuNTFoC";
 
-// url from zipcode converter
-let zipCodeConvert =
-  "https://www.zipcodeapi.com/rest/" +
-  zipcodeKey +
-  "/info.json/" +
-  zipcode +
-  "/degrees";
+function buildZipCodeRequestURI(zipCode) {
+  // zipcode key from converter
+  let zipcodeKey =
+    "yJ2trlSNPpRXf9kpE7yo6JUn4z9BxJdvAMSK5XuJXn9my5Ksec22lfAMgZxpLNeM";
 
+  // url from zipcode converter
+  let url =
+    "https://www.zipcodeapi.com/rest/" +
+    zipcodeKey +
+    "/info.json/" +
+    zipCode +
+    "/degrees";
 
-  // function that ingnites the map
+  return url;
+}
+
+// $("#inputZip")
+// function zipcode() {
+
+// };
+
+// stores when a change of zipcode is changed
+$("#inputZip").change(function() {
+  currentZipCode = $(this).val();
+  console.log(currentZipCode, "zip code");
+});
+
+$("#zipForm").submit(function(event) {
+  event.preventDefault();
+  // console.log();
+  if (!currentZipCode) {
+    return;
+  }
+
+ var zipCodeUrl = buildZipCodeRequestURI(currentZipCode);
+
+  // ajax call
+  $.ajax({
+    url: zipCodeUrl,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response.lat);
+    console.log(response.lng);
+
+    
+  });
+});
+// document.getElementById("zipForm");
+
+// form.onsubmit = function(event) {
+//   event.preventDefault();
+//   console.log(event);
+//   console.log(event.target, "event.target");
+//   console.log(event.target.elements[0].value, "value");
+// };
+
+// function that ingnites the map
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8
-  }); 
-
-}
-// when news-button is clicked
-
-$(document).on("click", "news-button", function() {
-
-  var newsButton = $(this).getElementById("news-button");
-
-
-// ajax call
-  $.ajax({
-    url: zipCodeConvert,
-    method: "GET"
-  }).then(function(response){
-    console.log(response.lat);
-    });
   });
-
+}
 
 // zipcode to lang convert API
 
 // https://www.zipcodeapi.com/rest/<api_key>/info.<format>/<zip_code>/<units>
 
-// api key
-// 8FiYHj3y9Don2RS4h6Q3q72bywLcJFAOWdzrASt2Dch9LA0gsRE3V9OGwTuNTFoC
+// convert api key
+// yJ2trlSNPpRXf9kpE7yo6JUn4z9BxJdvAMSK5XuJXn9my5Ksec22lfAMgZxpLNeM
